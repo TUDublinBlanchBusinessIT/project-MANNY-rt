55deffc08f4f9c548b8d7be8ef420a29ae706b0b
MANNY-rt <124693496+MANNY-rt@users.noreply.github.com>
2023-11-29 23:06:43 +0000

    Create createDBV1.sql
    first babystep database

---
+++ createDBV1.sql
@@ -0,0 +1,8 @@
+CREATE DATABASE IF NOT EXISTS user_information;
+
+USE user_information;
+
+INSERT INTO user (firstname, surname, dateofbirth, email)
+VALUES ('Adam', 'murphy', '2003-03-12', 'murphy@example.com');
+
+SELECT * FROM user;
