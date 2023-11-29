6d18612224ac96aa628bdbaac465563334d1f170
MANNY-rt <124693496+MANNY-rt@users.noreply.github.com>
2023-11-29 23:43:29 +0000

    Update createDBV1.sql
    updated

--- createDBV1.sql
+++ createDBV1.sql
@@ -1,8 +1,6 @@
-CREATE DATABASE IF NOT EXISTS user_information;
-
 USE user_information;
 
 INSERT INTO user (firstname, surname, dateofbirth, email)
 VALUES ('Adam', 'murphy', '2003-03-12', 'murphy@example.com');
 
-SELECT * FROM user;
+
