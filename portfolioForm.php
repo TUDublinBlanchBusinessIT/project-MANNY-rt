cd911392f38e57cf857b2b8c40fbb44c7d9ec74a
MANNY-rt <B00143972@mytudublin.ie>
2023-12-01 15:38:46 +0000

    updated html connection to database

--- portfolioForm.php
+++ portfolioForm.php
@@ -3,11 +3,16 @@ $fn = $_POST['fname'];
 $sn = $_POST['sname'];
 $dob = $_POST['dob'];
 $ema = $_POST['email'];
+$skills = $_POST['skills'];
+$education = $_POST['education'];
+$experience = $_POST['experience'];
+$homeAddress = $_POST['homeAddress'];
+$phoneNumber = $_POST['phoneNumber'];
 
 include("dbcon.php");
 
-$sql = "INSERT INTO user (firstname, surname, dateofbirth, email)
-VALUES ('$fn', '$sn', '$dob', '$ema')";
+$sql = "INSERT INTO user (firstname, surname, dateofbirth, email, skills, education, experience, phoneNumber, homeAddress)
+VALUES ('$fn', '$sn', '$dob', '$ema', '$skills', '$education', '$experience', '$phoneNumber', '$homeAddress')";
 
 //echo $sql;
 mysqli_query($conn, $sql);
